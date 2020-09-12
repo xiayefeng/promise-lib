46b30443e402c6299c9d7aa68d43bcb3a8f5444b
xiayefeng <xiayu_12@yeah.net>
2020-09-12 22:07:38 +0800

    Update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,1 +1,15 @@
 declare module '*.json'
+/* declare module "*.json"
+{ const value: {
+   version: string,
+   name: string
+  };
+  export default value;
+}
+
+// This will allow you to load JSON from remote URL responses
+
+declare module "json!*"
+{ const value: any;
+  export default value;
+} */
